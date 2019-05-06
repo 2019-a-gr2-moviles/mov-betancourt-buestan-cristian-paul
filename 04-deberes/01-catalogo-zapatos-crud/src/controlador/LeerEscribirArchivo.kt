518965820eab4ca015f63f159cd300c7d991f762
Cristian Betancourt <cristianbb98@gmail.com>
2019-05-06 18:06:25 -0500

    Cambio de ruta de creación y lectura de archivos

--- 04-deberes/01-catalogo-zapatos-crud/src/controlador/LeerEscribirArchivo.kt
+++ 04-deberes/01-catalogo-zapatos-crud/src/controlador/LeerEscribirArchivo.kt
@@ -2,19 +2,13 @@ package controlador
 
 import modelo.Compra
 import modelo.Zapato
-import java.io.IOException
-import java.io.FileNotFoundException
-import java.io.FileInputStream
-import java.io.ObjectInputStream
-import java.io.FileOutputStream
-import java.io.ObjectOutputStream
-import java.io.Serializable
+import java.io.*
 
 
 class LeerEscribirArchivo {
 
     companion object {
-        private var rutaArchivo: String = ""
+        private var rutaArchivo: String = "src${File.separatorChar}archivos${File.separatorChar}"
         private var nombreArchivoZapatos: String = "listaZapatos.zp"
         private var nombreArchivoCompras: String = "listaCompras.zp"
 
