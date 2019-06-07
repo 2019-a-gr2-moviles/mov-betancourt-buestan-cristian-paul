5fcb4e7fd9bda00edd08e39fff2e8bc37a82e3f6
Cristian Betancourt <cristianbb98@gmail.com>
2019-06-07 15:45:20 -0500

    Creacion recycler view

---
+++ 03-android/Aplicacion2/app/src/main/java/com/example/aplicacion2/Persona.kt
@@ -0,0 +1,8 @@
+package com.example.aplicacion2
+
+class Persona(
+    var nombre: String,
+    var cedula: String
+) {
+
+}
---
+++ 03-android/Aplicacion2/app/src/main/java/com/example/aplicacion2/ReciclerViewActivity.kt
@@ -0,0 +1,34 @@
+package com.example.aplicacion2
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v7.widget.DefaultItemAnimator
+import android.support.v7.widget.LinearLayoutCompat
+import android.support.v7.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_recicler_view.*
+import kotlinx.android.synthetic.main.layout.*
+import kotlin.random.Random
+
+class ReciclerViewActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recicler_view)
+
+        val recyclerView = rv_personas
+        val actividad = this
+        val lista = arrayListOf<Persona>()
+        lista.add(Persona("Cristian", "123485746"))
+        lista.add(Persona("Paul, ", "879824154"))
+
+        val adaptadorPersona = AdaptadorPersona(lista, actividad, recyclerView)
+        rv_personas.adapter = adaptadorPersona
+        rv_personas.itemAnimator = DefaultItemAnimator()
+        rv_personas.layoutManager = LinearLayoutManager(this)
+
+        adaptadorPersona.notifyDataSetChanged()
+
+    }
+
+
+}
