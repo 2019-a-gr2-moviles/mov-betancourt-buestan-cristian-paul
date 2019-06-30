a33f0b34caa5b02defa5631d462c83c3a5e5b454
Cristian Betancourt <cristianbb98@gmail.com>
2019-06-29 22:47:31 -0500

    Creacion de menu de zapato cliente y compras

---
+++ 04-deberes/02-proyecto/AppZapatos/app/src/main/java/com/example/appzapatos/modulo_clientes/MenuClientes.kt
@@ -0,0 +1,13 @@
+package com.example.appzapatos.modulo_clientes
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.appzapatos.R
+
+class MenuClientes : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu_clientes)
+    }
+}
---
+++ 04-deberes/02-proyecto/AppZapatos/app/src/main/java/com/example/appzapatos/modulo_zapatos/MenuZapatos.kt
@@ -0,0 +1,13 @@
+package com.example.appzapatos.modulo_zapatos
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.appzapatos.R
+
+class MenuZapatos : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu_zapatos)
+    }
+}
